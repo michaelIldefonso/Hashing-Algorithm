b50aba59ed5ae65eb0e998dbe6276e4cabfc1463
Jonathan Delda <139674674+jonathansison@users.noreply.github.com>
2024-10-31 01:08:16 +0800

    Update script.js

--- documentation/script.js
+++ documentation/script.js
@@ -7,10 +7,21 @@ function smoothScrollToSection(sectionId) {
 
 document.querySelectorAll('.navbar ul li a').forEach(link => {
     link.addEventListener('click', function(event) {
-        event.preventDefault();
+        if (this.classList.contains('try-it-link')) {
+            return; 
+        }
+
+        event.preventDefault(); 
 
         const sectionId = this.getAttribute('href').substring(1); 
-
         smoothScrollToSection(sectionId);
+    });
+});
+
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        if (!link.classList.contains('try-it-link')) {
+            event.preventDefault(); 
+        }
     });
 });
