76db241307d21d1b3d7ec2edf1e9874b731b1a8c
Jonathan Delda <139674674+jonathansison@users.noreply.github.com>
2024-10-31 20:00:38 +0800

    Update script.js

--- documentation/script.js
+++ documentation/script.js
@@ -2,7 +2,7 @@ function smoothScrollToSection(sectionId) {
     const targetSection = document.getElementById(sectionId);
     const headerOffset = document.querySelector('header').offsetHeight; 
     const targetPosition = targetSection.getBoundingClientRect().top + window.scrollY - headerOffset
-                            - (window.innerHeight / 2) + (targetSection.offsetHeight / 2)-50;
+                            - (window.innerHeight / 2) + (targetSection.offsetHeight / 2) - 50;
 
     window.scrollTo({
         top: targetPosition,
@@ -17,22 +17,43 @@ document.querySelectorAll('.navbar ul li a').forEach(link => {
         }
 
         event.preventDefault(); 
-
         const sectionId = this.getAttribute('href').substring(1); 
         smoothScrollToSection(sectionId);
+        
+        navbar.classList.remove('active'); 
+        burger.classList.remove('hidden'); 
     });
 });
 
-document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', (event) => {
-        if (!link.classList.contains('try-it-link')) {
-            event.preventDefault(); 
-        }
-    });
-});
 const burger = document.querySelector('.burger');
 const navbar = document.querySelector('.navbar');
+const closeBtn = document.querySelector('.close');
 
 burger.addEventListener('click', () => {
-    navbar.classList.toggle('active'); // Toggle the navbar visibility
+    navbar.classList.toggle('active'); 
+    burger.classList.add('hidden'); 
+});
+
+closeBtn.addEventListener('click', () => {
+    navbar.classList.remove('active'); 
+    burger.classList.remove('hidden'); 
+});
+
+window.addEventListener('scroll', () => {
+    if (navbar.classList.contains('active')) {
+    }
+});
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop) {
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
+    }
+
+    lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
 });
