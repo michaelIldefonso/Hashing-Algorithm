dd822ee833e7b676dad2663a406d6717b366a141
Michael Ildefonso <michaelildefonso8@gmail.com>
2024-11-04 21:25:46 +0800

    Update hashing.py

--- bisHash/hashing.py
+++ bisHash/hashing.py
@@ -5,25 +5,25 @@ import re
 
 PEPPER = "jdafhpoahsofdashjp"
 
-def is_strong_password(passW):
+def is_strong_password(password):
     # Check password length
-    if len(passW) < 12:
+    if len(password) < 12:
         return False, "Password must be at least 12 characters long."
 
     # Check for uppercase
-    if not re.search(r'[A-Z]', passW):
+    if not re.search(r'[A-Z]', password):
         return False, "Password must contain at least one uppercase letter."
 
     # Check for lowercase letter
-    if not re.search(r'[a-z]', passW):
+    if not re.search(r'[a-z]', password):
         return False, "Password must contain at least one lowercase letter."
 
     # Check for digit
-    if not re.search(r'[0-9]', passW):
+    if not re.search(r'[0-9]', password):
         return False, "Password must contain at least one digit."
 
     # Check for special character
-    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', passW):
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
         return False, "Password must contain at least one special character."
 
     return True, "Password is strong."
@@ -32,7 +32,7 @@ ONE_MINUTE = 60
 
 # Initialize the PasswordHasher with custom parameters
 ph = PasswordHasher(
-    time_cost=12,          # Number of iterations
+    time_cost=8,          # Number of iterations
     memory_cost=2 ** 16,    # Memory cost
     parallelism=1,          # Number of parallel threads
     hash_len=64,            # Length of the resulting hash
@@ -42,14 +42,14 @@ ph = PasswordHasher(
 # Rate-limited function to hash the password
 @sleep_and_retry  # Ensures that the function sleeps if the limit is reached
 @limits(calls=5, period=ONE_MINUTE)
-def bis_hash(name, passwrd):
-    combined_input = name + passwrd + PEPPER
+def bis_hash(email, passwrd):
+    combined_input = email + passwrd + PEPPER
     return ph.hash(combined_input)
 
 # Function to verify a password
-def verify_password(stored_hash, name, entered_password):
-    # Combine the username and entered password
-    combined_input = name + entered_password + PEPPER
+def verify_password(stored_hash, email, entered_password):
+    # Combine the email and entered password
+    combined_input = email + entered_password + PEPPER
     try:
         ph.verify(stored_hash, combined_input)
         return True
