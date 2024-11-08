38cb20346baf53a140caf1440b3fe4f9e9ec7a9e
Michael Ildefonso <michaelildefonso8@gmail.com>
2024-11-08 22:30:34 +0800

    aa

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bisHash',
-    version='0.26',
+    version='1.0.1',
     packages=find_packages(),
     description='A simple hashing algorithm library',
     long_description=open("README.md").read(),
